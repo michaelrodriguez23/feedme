41c8d127d8d153d98ff96388f9aa364bdd45a64e
Michael Rodriguez <michaelrodriguez23@gmail.com>
2022-03-31 10:40:30 -0400

    Updates

---
+++ precache-manifest.edad9a4b81e3b796130090f736ea7966.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8587adc63e0b23110afd",
+    "url": "/michael-elijah.com/static/css/main.fe1cd9ff.chunk.css"
+  },
+  {
+    "revision": "8587adc63e0b23110afd",
+    "url": "/michael-elijah.com/static/js/main.8587adc6.chunk.js"
+  },
+  {
+    "revision": "c6b5a3e0f92ee25b3986",
+    "url": "/michael-elijah.com/static/js/1.c6b5a3e0.chunk.js"
+  },
+  {
+    "revision": "db0a9a7459e257309c28",
+    "url": "/michael-elijah.com/static/js/2.db0a9a74.chunk.js"
+  },
+  {
+    "revision": "ab41a9a848dd17ec245a",
+    "url": "/michael-elijah.com/static/js/runtime~main.ab41a9a8.js"
+  },
+  {
+    "revision": "f5df081a594798faf65e0426d5890bec",
+    "url": "/michael-elijah.com/static/media/me.f5df081a.png"
+  },
+  {
+    "revision": "77f72554dab95a54e94332a4f89afe14",
+    "url": "/michael-elijah.com/static/media/apple_amp.77f72554.wsz"
+  },
+  {
+    "revision": "bdd531543a82b749ddceb3bb6e2fb9e5",
+    "url": "/michael-elijah.com/index.html"
+  }
+];
