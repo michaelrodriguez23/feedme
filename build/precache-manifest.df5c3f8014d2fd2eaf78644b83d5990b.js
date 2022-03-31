4bd3d8ca78ef9d9b583f9b1c42ca1bcfaa739351
Michael Rodriguez <michaelrodriguez23@gmail.com>
2022-03-31 12:44:57 -0400

    tok

---
+++ build/precache-manifest.df5c3f8014d2fd2eaf78644b83d5990b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d3abeecfc7ff8f2d1ea",
+    "url": "/static/css/main.4e282d1d.chunk.css"
+  },
+  {
+    "revision": "6d3abeecfc7ff8f2d1ea",
+    "url": "/static/js/main.6d3abeec.chunk.js"
+  },
+  {
+    "revision": "2d281c672a566deb1a05",
+    "url": "/static/js/1.2d281c67.chunk.js"
+  },
+  {
+    "revision": "1ff904fd706a9a5b1702",
+    "url": "/static/js/2.1ff904fd.chunk.js"
+  },
+  {
+    "revision": "7fbb040d89207e86c564",
+    "url": "/static/js/runtime~main.7fbb040d.js"
+  },
+  {
+    "revision": "c9e6d1006ad35d528cc1e2fcd3bafdf0",
+    "url": "/index.html"
+  }
+];
