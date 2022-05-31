065fba62798a8ee682c2c88f1a09d748b92d7097
Michael Rodriguez <michaelrodriguez23@gmail.com>
2022-05-31 13:58:59 -0400

    Updates

---
+++ precache-manifest.020ee586e617d6c0f1b919ba5e258df5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e6909a8633a262c7f30",
+    "url": "/static/css/main.a9318ada.chunk.css"
+  },
+  {
+    "revision": "0e6909a8633a262c7f30",
+    "url": "/static/js/main.0e6909a8.chunk.js"
+  },
+  {
+    "revision": "b3be8a9b1cc833812d75",
+    "url": "/static/js/1.b3be8a9b.chunk.js"
+  },
+  {
+    "revision": "2070f703159283d6cf58",
+    "url": "/static/js/2.2070f703.chunk.js"
+  },
+  {
+    "revision": "335750b4ee8447f9df02",
+    "url": "/static/js/runtime~main.335750b4.js"
+  },
+  {
+    "revision": "7fb06ce07d5d95a11b0aabf804679691",
+    "url": "/index.html"
+  }
+];
