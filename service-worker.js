ebf26fc800bf0ee4fddc03fa620138418e6c0e89
Michael Rodriguez <michaelrodriguez23@gmail.com>
2022-06-25 16:49:55 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5aefd48b7186cf232d9593f21864dcd8.js"
+  "/precache-manifest.fa1c262d3f40c46f0e8758e5467b009b.js"
 );
 
 workbox.clientsClaim();
